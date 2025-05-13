348b5c91986e1b1c9e650f1a75ddb561744a6d9a
William Madison <billymadison30@gmail.com>
2025-05-12 23:45:06 -0700

    Create config.py

---
+++ core/config.py
@@ -0,0 +1,13 @@
+KRAKEN_API_KEY = 'your_kraken_key'
+KRAKEN_SECRET = 'your_kraken_secret'
+ALPACA_API_KEY = 'your_alpaca_key'
+ALPACA_SECRET = 'your_alpaca_secret'
+ALPACA_BASE_URL = 'https://paper-api.alpaca.markets'  # or live url
+
+SYMBOLS_CRYPTO = ["BTC/USD", "ETH/USD"]
+SYMBOLS_STOCKS = ["AAPL", "MSFT"]
+
+MIDI_NOTE_RANGE = (48, 72)
+BASE_VELOCITY = 80
+RSI_PERIOD = 14
+TREND_WINDOW = 10
